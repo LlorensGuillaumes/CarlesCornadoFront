ef20c21d0f37dfae731a7da52dfd148e49df4aa5
LlorensGuillaumes <jllorensguillaumes@gmail.com>
2023-11-15 08:58:05 +0100

    feat: home styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,25 +5,25 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
     const navigate = useNavigate();
 return (
-    <div>
-    <div className="link">
+    <div className="home_container">
+    <div className="link home_item">
     <h1>CLIENTS</h1>
     </div>
-    <div className="link">
+    <div className="link home_item">
     <h1 onClick={()=>{
         navigate('/providers')
     }}>PROVEÏDORS</h1>
     </div>
-    <div className="link">
+    <div className="link home_item">
     <h1 onClick={()=>{navigate('/components')}}>COMPONENTS</h1>
     </div>
-    <div className="link">
+    <div className="link home_item">
     <h1 onClick={()=>{navigate('/products')}}>PRODUCTES</h1>
     </div>
-    <div className="link">
+    <div className="link home_item">
     <h1>FACTURES</h1>
     </div>
-    <div className="link">
+    <div className="link home_item">
     <h1 onClick={()=>{navigate('/processes')}}>PROCESSOS</h1>
     </div>
     </div>
